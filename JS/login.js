1818dda699698d15585584be6dc07b5cad3e7f31
Herix90 <118936912+Herix90@users.noreply.github.com>
2023-04-17 18:02:19 -0600

    Create login.js

---
+++ JS/login.js
@@ -0,0 +1,19 @@
+var clientesRef = firebase.firestore().collection("Clientes");
+document.querySelector("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var usuario = document.querySelector('input[name="USUARIO"]').value;
+  var password = document.querySelector('input[name="PASSWORD"]').value;
+
+  clientesRef.where("EmailCliente", "==", usuario).where("PassCliente", "==", password).get().then(function(querySnapshot) {
+    if (querySnapshot.size > 0) {
+      alert("Bienvenido");
+      window.location.href = "RegistroUsuario.html";
+    } else {
+      alert("Usuario o contraseña incorrectos");
+    }
+  }).catch(function(error) {
+    alert("Ocurrió un error al buscar en la base de datos");
+    console.error(error);
+  });
+});
